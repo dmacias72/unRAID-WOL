3c1d370dccd881413b87ec7d8cf7fa046e94c0a1
dmacias72 <derekmacias@live.com>
2017-07-23 04:46:25 -0600

    move scan log to /var/log

--- source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/scan.php
+++ source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/scan.php
@@ -1,9 +1,8 @@
 <?
-    $log = '/var/log/wakeonlan/scan.xml';
-    if (file_exists($log))
-        file_put_contents($log, '');
+    $log = '/var/log/scan';
+    file_put_contents($log, '');
     $ip = $_POST['ip'];
     $net = substr_replace($ip ,'',-1).'0/24';
-    $cmd = "/usr/bin/nmap -sn -oX $log --exclude ".escapeshellarg($ip).' '.escapeshellarg($net);
+    $cmd = "/usr/bin/nmap -sn -oX $log --exclude ".escapeshellarg($ip).' '.escapeshellarg($net). ' > /dev/null 2>&1';
     shell_exec($cmd);
 ?>
