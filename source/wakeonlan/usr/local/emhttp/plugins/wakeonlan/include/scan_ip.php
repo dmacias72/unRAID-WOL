1b97f5cc62605a591c1ec8a9d103470a867af019
dmacias72 <derekmacias@live.com>
2019-12-30 20:17:34 -0700

    change status icons to fa fonts

--- source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/scan_ip.php
+++ source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/scan_ip.php
@@ -1,6 +1,6 @@
 <?
     $ip = escapeshellarg($_GET['ip']);
-    $cmd = "/bin/ping -b -c 1 -i 1 -W 1 $ip | grep 'received' | awk -F',' '{ print $2}' | grep '1 received' && echo 'on' || echo 'blink'";
+    $cmd = "/bin/ping -b -c 1 -i 1 -W 1 $ip | grep 'received' | awk -F',' '{ print $2}' | grep '1 received' && echo 'green' || echo 'grey'";
     $status = trim(exec($cmd));
     echo json_encode($status);
 ?>
